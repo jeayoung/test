cc8c36e8d7d8de5da624f913a8708e7e564ba6c5
jeayoung <92rabit@naver.com>
2014-04-08 19:41:28 +0900

    hw1.cpp

---
+++ hw1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+void sum(ifstream& in, ofstream& out, int& num);
+
+void sum(ifstream& in, ofstream& out, int& num){
+	num+=1;
+	out<<num<<endl;
+}
+void main(){
+	int num;
+	ifstream in;
+	in.open("input.txt");
+	
+	ofstream out("input.txt",ios::app);
+	out<<endl;
+
+	for(int i=0; i<100; i++){
+	while(in.eof()){
+		in>>num;
+	}
+	sum(in,out,num);
+	}
+	out.close();
+
+}
